7e07b8d0e17580c6d1450a9416c58bd20469baf7
Stephan Raab <stephan.raab@nyhart.com>
2016-10-07 15:47:09 -0400

    made it through 1/3

---
+++ emberTaskTut/app/controllers/tasks/new.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        addTask: function() {
+            var title = this.get('title');
+            var date = this.get('date');
+            var description = this.get('description');
+
+            //Create new task
+               
+        }
+    }
+});
